8a7f5cbe97690c77f5ac0aafaad94523210537fc
Juliana Boudakian <julianal.b@hotmail.com>
2019-03-14 13:36:05 +0000

    Max and min temp

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (longitude, latitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location!', undefined);
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It's currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain`);
+            callback(undefined, `${body.daily.data[0].summary} Max temperature of ${body.daily.data[0].apparentTemperatureHigh} and min of ${body.daily.data[0].apparentTemperatureLow}.`);
         }
     });
 
